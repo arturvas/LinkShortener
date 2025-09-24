0127de03fbafbdd4cdd9def40edecafb8a9e4174
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2025-09-24 16:18:29 -0400

    feat: add database integration and improve URL shortening logic

--- LinkShortener/Program.cs
+++ LinkShortener/Program.cs
@@ -1,9 +1,18 @@
+using LinkShortener;
+using LinkShortener.Entities;
 using LinkShortener.Models;
+using LinkShortener.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("database")));
+
+builder.Services.AddScoped<UrlShorteningService>();
 
 var app = builder.Build();
 
@@ -13,14 +22,33 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost("api/shorten", (ShortenUrlRequest request) =>
+app.MapPost("api/shorten", async (
+    ShortenUrlRequest request,
+    UrlShorteningService service,
+    ApplicationDbContext dbContext,
+    HttpContext httpContext) =>
 {
     if (!Uri.TryCreate(request.Url, UriKind.Absolute, out _))
     {
         return Results.BadRequest("The specified URL is not valid.");
     }
     
+    var code = await service.GenerateUniqueCode();
+
+    var shortenUrl = new ShortenedUrl
+    {
+        Id = Guid.NewGuid(),
+        LongUrl = request.Url,
+        Code = code,
+        ShortUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}/api/{code}",
+        CreatedAt = DateTimeOffset.UtcNow
+    };
     
+    dbContext.ShortenedUrls.Add(shortenUrl);
+    
+    await dbContext.SaveChangesAsync();
+    
+    return Results.Ok(shortenUrl.ShortUrl);
 });
 
 app.UseHttpsRedirection();
